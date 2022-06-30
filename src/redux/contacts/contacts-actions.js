84fde7ed4ff51f132d87d4886b21d89080e1d143
BogdanV <zarazailo@gmail.com>
2022-06-30 15:55:53 +0300

    start

---
+++ src/redux/contacts/contacts-actions.js
@@ -0,0 +1,36 @@
+import { nanoid } from 'nanoid'
+import { createAction } from '@reduxjs/toolkit'
+
+const addContact = createAction('contacts/Add', (name, number) => ({
+    payload: {
+        id: nanoid(6),
+        name,
+        number,
+    },
+}));
+
+const deleteContact = createAction('contacts/Delete');
+
+const changeFilter = createAction('contacts/Filter');
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default { addContact, deleteContact, changeFilter };
+
+// const addContact = (name, number) => ({
+//     type: types.ADD,
+//     payload: {
+//         id: nanoid(6),
+//         name,
+//         number,
+//     }
+// })
+
+// const deleteContact = contactId => ({
+//     type: types.DELETE,
+//     payload: contactId,
+// })
+
+// const changeFilter = value => ({
+//     type: types.CHANGE_FILTER,
+//     payload:value,
+// })
